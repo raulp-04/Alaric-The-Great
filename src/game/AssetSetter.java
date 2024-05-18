0d8c3ca808a33a76255442006faec8a234e31258
raulp-04 <raul.parasca@gmail.com>
2024-05-18 15:01:52 +0300

    Small fixes

--- src/game/AssetSetter.java
+++ src/game/AssetSetter.java
@@ -96,71 +96,88 @@ public class AssetSetter {
         gp.monsterArray[mapNum][0] = new MON_Skeleton(gp);
         gp.monsterArray[mapNum][0].worldX = 32 * gp.tileSize;
         gp.monsterArray[mapNum][0].worldY = 22 * gp.tileSize;
+        gp.monsterArray[mapNum][0].life = gp.monsterArray[mapNum][0].maxLife;
 
         gp.monsterArray[mapNum][1] = new MON_Skeleton(gp);
         gp.monsterArray[mapNum][1].worldX = 27 * gp.tileSize;
         gp.monsterArray[mapNum][1].worldY = 19 * gp.tileSize;
+        gp.monsterArray[mapNum][1].life = gp.monsterArray[mapNum][0].maxLife;
 
         gp.monsterArray[mapNum][2] = new MON_Skeleton(gp);
         gp.monsterArray[mapNum][2].worldX = 39 * gp.tileSize;
         gp.monsterArray[mapNum][2].worldY = 24 * gp.tileSize;
+        gp.monsterArray[mapNum][2].life = gp.monsterArray[mapNum][0].maxLife;
 
         gp.monsterArray[mapNum][3] = new MON_Skeleton(gp);
         gp.monsterArray[mapNum][3].worldX = 23 * gp.tileSize;
         gp.monsterArray[mapNum][3].worldY = 22 * gp.tileSize;
+        gp.monsterArray[mapNum][3].life = gp.monsterArray[mapNum][0].maxLife;
 
         gp.monsterArray[mapNum][4] = new MON_Skeleton(gp);
         gp.monsterArray[mapNum][4].worldX = 28 * gp.tileSize;
         gp.monsterArray[mapNum][4].worldY = 18 * gp.tileSize;
+        gp.monsterArray[mapNum][4].life = gp.monsterArray[mapNum][0].maxLife;
 
         gp.monsterArray[mapNum][5] = new MON_Skeleton(gp);
         gp.monsterArray[mapNum][5].worldX = 34 * gp.tileSize;
         gp.monsterArray[mapNum][5].worldY = 25 * gp.tileSize;
+        gp.monsterArray[mapNum][5].life = gp.monsterArray[mapNum][0].maxLife;
 
         gp.monsterArray[mapNum][6] = new MON_Skeleton(gp);
         gp.monsterArray[mapNum][6].worldX = 33 * gp.tileSize;
         gp.monsterArray[mapNum][6].worldY = 22 * gp.tileSize;
+        gp.monsterArray[mapNum][6].life = gp.monsterArray[mapNum][0].maxLife;
 
         gp.monsterArray[mapNum][7] = new MON_Skeleton(gp);
         gp.monsterArray[mapNum][7].worldX = 30 * gp.tileSize;
         gp.monsterArray[mapNum][7].worldY = 13 * gp.tileSize;
+        gp.monsterArray[mapNum][7].life = gp.monsterArray[mapNum][0].maxLife;
 
         gp.monsterArray[mapNum][8] = new MON_Skeleton(gp);
         gp.monsterArray[mapNum][8].worldX = 23 * gp.tileSize;
         gp.monsterArray[mapNum][8].worldY = 13 * gp.tileSize;
+        gp.monsterArray[mapNum][8].life = gp.monsterArray[mapNum][0].maxLife;
 
         gp.monsterArray[mapNum][9] = new MON_Skeleton(gp);
         gp.monsterArray[mapNum][9].worldX = 26 * gp.tileSize;
         gp.monsterArray[mapNum][9].worldY = 15 * gp.tileSize;
+        gp.monsterArray[mapNum][9].life = gp.monsterArray[mapNum][0].maxLife;
 
         mapNum = 3;
         gp.monsterArray[mapNum][0] = new MON_Goblin(gp);
         gp.monsterArray[mapNum][0].worldX = 25 * gp.tileSize;
         gp.monsterArray[mapNum][0].worldY = 11 * gp.tileSize;
+        gp.monsterArray[mapNum][0].life = gp.monsterArray[mapNum][0].maxLife;
 
         gp.monsterArray[mapNum][1] = new MON_Goblin(gp);
         gp.monsterArray[mapNum][1].worldX = 39 * gp.tileSize;
         gp.monsterArray[mapNum][1].worldY = 10 * gp.tileSize;
+        gp.monsterArray[mapNum][1].life = gp.monsterArray[mapNum][0].maxLife;
 
         gp.monsterArray[mapNum][2] = new MON_Goblin(gp);
         gp.monsterArray[mapNum][2].worldX = 33 * gp.tileSize;
         gp.monsterArray[mapNum][2].worldY = 18 * gp.tileSize;
+        gp.monsterArray[mapNum][2].life = gp.monsterArray[mapNum][0].maxLife;
 
         gp.monsterArray[mapNum][3] = new MON_Goblin(gp);
         gp.monsterArray[mapNum][3].worldX = 24 * gp.tileSize;
         gp.monsterArray[mapNum][3].worldY = 28 * gp.tileSize;
+        gp.monsterArray[mapNum][3].life = gp.monsterArray[mapNum][0].maxLife;
 
         mapNum = 4;
         gp.monsterArray[mapNum][0] = MON_Boss.getSingleInstance();
         gp.monsterArray[mapNum][0].worldX = 26 * gp.tileSize;
         gp.monsterArray[mapNum][0].worldY = 12 * gp.tileSize;
+        gp.monsterArray[mapNum][0].life = gp.monsterArray[mapNum][0].maxLife;
 
         gp.monsterArray[mapNum][1] = new MON_Goblin(gp);
         gp.monsterArray[mapNum][1].worldX = 27 * gp.tileSize;
         gp.monsterArray[mapNum][1].worldY = 24 * gp.tileSize;
+        gp.monsterArray[mapNum][1].life = gp.monsterArray[mapNum][1].maxLife;
 
         gp.monsterArray[mapNum][2] = new MON_Goblin(gp);
         gp.monsterArray[mapNum][2].worldX = 39 * gp.tileSize;
         gp.monsterArray[mapNum][2].worldY = 24 * gp.tileSize;
+        gp.monsterArray[mapNum][2].life = gp.monsterArray[mapNum][2].maxLife;
     }
 }
